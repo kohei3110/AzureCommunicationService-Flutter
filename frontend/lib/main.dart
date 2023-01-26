f573eed707ad42829dcbf23008bf30e5ff3fb83d
kohei3110 <kk.31108424@gmail.com>
2023-01-26 16:15:54 +0900

    バックグラウンドで動作確認

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -83,8 +83,6 @@ void _firebaseMessagingForegroundHandler(
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-
-  print("Handling a background message: ${message.messageId}");
   showCallkitIncoming(message.messageId!);
 }
 
