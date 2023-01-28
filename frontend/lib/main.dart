04f774f7dadcc90fef14e7a7ff2b6e54a680ed66
kohei3110 <kk.31108424@gmail.com>
2023-01-28 21:15:22 +0900

    イベントハンドラ追加

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:azurecommunicationserviceflutter/views/widgets/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_callkit_incoming/entities/android_params.dart';
+import 'package:flutter_callkit_incoming/entities/call_event.dart';
 import 'package:flutter_callkit_incoming/entities/call_kit_params.dart';
 import 'package:flutter_callkit_incoming/entities/ios_params.dart';
 import 'package:flutter_callkit_incoming/flutter_callkit_incoming.dart';
@@ -87,6 +88,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 }
 
 Future<void> showCallkitIncoming(String uuid) async {
+  // FIXME: パラメータを provider 経由で取得する
   final params = CallKitParams(
     id: uuid,
     nameCaller: 'Kohei Saito',
@@ -129,4 +131,48 @@ Future<void> showCallkitIncoming(String uuid) async {
     ),
   );
   await FlutterCallkitIncoming.showCallkitIncoming(params);
+  FlutterCallkitIncoming.onEvent.listen((CallEvent? event) {
+    switch (event!.event) {
+      case Event.ACTION_CALL_INCOMING:
+        print("ACTION_CALL_INCOMING");
+        break;
+      case Event.ACTION_CALL_START:
+        // TODO: started an outgoing call
+        // TODO: show screen calling in Flutter
+        break;
+      case Event.ACTION_CALL_ACCEPT:
+        print("電話に出る");
+        break;
+      case Event.ACTION_CALL_DECLINE:
+        // TODO: declined an incoming call
+        break;
+      case Event.ACTION_CALL_ENDED:
+        // TODO: ended an incoming/outgoing call
+        break;
+      case Event.ACTION_CALL_TIMEOUT:
+        // TODO: missed an incoming call
+        break;
+      case Event.ACTION_CALL_CALLBACK:
+        // TODO: only Android - click action `Call back` from missed call notification
+        break;
+      case Event.ACTION_CALL_TOGGLE_HOLD:
+        // TODO: only iOS
+        break;
+      case Event.ACTION_CALL_TOGGLE_MUTE:
+        // TODO: only iOS
+        break;
+      case Event.ACTION_CALL_TOGGLE_DMTF:
+        // TODO: only iOS
+        break;
+      case Event.ACTION_CALL_TOGGLE_GROUP:
+        // TODO: only iOS
+        break;
+      case Event.ACTION_CALL_TOGGLE_AUDIO_SESSION:
+        // TODO: only iOS
+        break;
+      case Event.ACTION_DID_UPDATE_DEVICE_PUSH_TOKEN_VOIP:
+        // TODO: only iOS
+        break;
+    }
+  });
 }
